dd0e3c0d3400f275ad546bd78834a70d56e8f74a
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-09-12 17:20:28 +0800

    feat: add Spring Boot application entrypoint

---
+++ backend/src/main/java/com/thecodinganalyst/staffalias/StaffAliasApplication.java
@@ -0,0 +1,12 @@
+package com.thecodinganalyst.staffalias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffAliasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffAliasApplication.class, args);
+    }
+}
