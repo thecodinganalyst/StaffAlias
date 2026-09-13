f3c9287aed90d8f18fe7fd86e722a17609fe392f
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-09-13 18:33:54 +0800

    feat: enable required GCP APIs

---
+++ infrastructure/terraform/apis.tf
@@ -0,0 +1,19 @@
+locals {
+  required_services = toset([
+    "artifactregistry.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sts.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "required" {
+  for_each = local.required_services
+
+  project            = var.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
