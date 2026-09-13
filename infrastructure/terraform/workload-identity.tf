de5fe3acd11b5f41139fc4e78c44d6ef42fc500c
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-09-13 19:36:34 +0800

    infra: add GitHub Workload Identity Federation

---
+++ infrastructure/terraform/workload-identity.tf
@@ -0,0 +1,41 @@
+resource "google_iam_workload_identity_pool" "github" {
+  project                   = var.project_id
+  workload_identity_pool_id = var.github_workload_identity_pool_id
+  display_name              = "StaffAlias GitHub Actions"
+  description               = "OIDC trust for production deployments from the StaffAlias repository"
+
+  depends_on = [google_project_service.required]
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = var.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = var.github_workload_identity_provider_id
+  display_name                       = "StaffAlias GitHub"
+  description                        = "Accepts GitHub Actions OIDC tokens only for StaffAlias production deployments"
+
+  attribute_mapping = {
+    "google.subject"                = "assertion.sub"
+    "attribute.repository_id"       = "assertion.repository_id"
+    "attribute.repository_owner_id" = "assertion.repository_owner_id"
+    "attribute.ref"                 = "assertion.ref"
+    "attribute.environment"         = "assertion.environment"
+  }
+
+  attribute_condition = join(" && ", [
+    "assertion.repository_id == '${var.github_repository_id}'",
+    "assertion.repository_owner_id == '${var.github_repository_owner_id}'",
+    "assertion.ref == 'refs/heads/${var.github_deploy_branch}'",
+    "assertion.environment == '${var.github_environment}'",
+  ])
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com/"
+  }
+}
+
+resource "google_service_account_iam_member" "github_deployment_workload_identity_user" {
+  service_account_id = google_service_account.deployment.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository_id/${var.github_repository_id}"
+}
