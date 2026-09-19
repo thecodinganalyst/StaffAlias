d2f36d6e6c8d36365087ee516279df872c68d572
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-09-19 16:09:34 +0800

    Fix issue #72 pipeline syntax and tests

--- infrastructure/terraform/secret-manager.tf
+++ infrastructure/terraform/secret-manager.tf
@@ -3,7 +3,8 @@ locals {
     "staffalias-db-url",
     "staffalias-db-username",
     "staffalias-db-password",
-    "staffalias-platform-admin-password",\n    "staffalias-resend-api-key",
+    "staffalias-platform-admin-password",
+    "staffalias-resend-api-key",
   ])
 }
 
